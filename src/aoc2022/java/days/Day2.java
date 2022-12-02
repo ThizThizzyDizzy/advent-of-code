1f0b231f688e3f26981e27b6c664edb378e61232
ThizThizzyDizzy <thizthizzydizzy@gmail.com>
2022-12-02 05:43:55 -0900

    Day 2 Part 2 (Java)

--- src/aoc2022/java/days/Day2.java
+++ src/aoc2022/java/days/Day2.java
@@ -15,6 +15,12 @@ public class Day2 extends Day{
             total+=result;
         }
         System.out.println(total);
+        total = 0;
+        for(String s : games){
+            int result = rps2(s);
+            total+=result;
+        }
+        System.out.println(total);
     }
     private int rps(String s) {
         int opponent = switch(s.charAt(0)){
@@ -29,6 +35,27 @@ public class Day2 extends Day{
             case 'Z' -> 3;
             default -> 0;
         };
+        int roundScore = 0;
+        if(me==opponent+1||me==opponent-2)roundScore = 6;
+        else if(me==opponent)roundScore = 3;
+        else roundScore = 0;
+        return roundScore+me;
+    }
+    private int rps2(String s) {
+        int opponent = switch(s.charAt(0)){
+            case 'A' -> 1;
+            case 'B' -> 2;
+            case 'C' -> 3;
+            default -> 0;
+        };
+        int me = switch(s.charAt(2)){
+            case 'X' -> opponent-1;
+            case 'Y' -> opponent;
+            case 'Z' -> opponent+1;
+            default -> 0;
+        };
+        if(me<1)me+=3;
+        if(me>3)me-=3;
         int roundScore = 0;
         if(me==opponent+1||me==opponent-2)roundScore = 6;
         else if(me==opponent)roundScore = 3;
