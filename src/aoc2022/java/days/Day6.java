add8728ae417e9907f699848e84c1957666828e1
ThizThizzyDizzy <thizthizzydizzy@gmail.com>
2022-12-06 11:32:36 -0500

    Day 6 Part 2 - Java

--- src/aoc2022/java/days/Day6.java
+++ src/aoc2022/java/days/Day6.java
@@ -8,15 +8,32 @@ public class Day6 extends Day{
     @Override
     public void run() {
         char[] chars = input.toCharArray();
+        boolean part1 = false;
         for(int i = 3; i<chars.length; i++){
             HashSet<Character> set = new HashSet<>();
             set.add(chars[i]);
             set.add(chars[i-1]);
             set.add(chars[i-2]);
             set.add(chars[i-3]);
-            if(set.size()==4){
+            if(set.size()>=4&&!part1){
                 System.out.println(i+1);
-                break;
+                part1 = true;
+            }
+            if(i>13){
+                set.add(chars[i-4]);
+                set.add(chars[i-5]);
+                set.add(chars[i-6]);
+                set.add(chars[i-7]);
+                set.add(chars[i-8]);
+                set.add(chars[i-9]);
+                set.add(chars[i-10]);
+                set.add(chars[i-11]);
+                set.add(chars[i-12]);
+                set.add(chars[i-13]);
+                if(set.size()>=14){
+                    System.out.println(i+1);
+                    return;
+                }
             }
         }
     }
