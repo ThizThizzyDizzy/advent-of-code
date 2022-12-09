7226c8ab620a923a9887e67bfafa71f5fa4146c1
ThizThizzyDizzy <thizthizzydizzy@gmail.com>
2022-12-09 08:08:19 -0500

    Day 1 Part 2 - Java

--- src/aoc2022/java/days/Day9.java
+++ src/aoc2022/java/days/Day9.java
@@ -7,14 +7,18 @@ public class Day9 extends Day{
     }
     @Override
     public void run(){
-        int hx = 0, hy = 0;
-        int tx = 0, ty = 0;
+        sim(2);
+        sim(10);
+    }
+    public void sim(int len){
+        int[] rx = new int[len];
+        int[] ry = new int[len];
         HashSet<String> visited = new HashSet<>();
-        visited.add(tx+","+ty);
+        visited.add(rx[len-1]+","+ry[len-1]);
         for(String s : input.split("\n")){
             char dir = s.charAt(0);
-            int len = Integer.parseInt(s.substring(2));
-            for(int i = 0; i<len; i++){
+            int dist = Integer.parseInt(s.substring(2));
+            for(int i = 0; i<dist; i++){
                 int dx = switch(dir){
                     case 'L' -> -1;
                     case 'R' -> 1;
@@ -25,18 +29,25 @@ public class Day9 extends Day{
                     case 'D' -> 1;
                     default -> 0;
                 };
-                hx+=dx;
-                hy+=dy;
-                int dist = Math.max(Math.abs(hx-tx),Math.abs(hy-ty));
-                if(dist>1){//too far away! move closer!
-                    boolean Y = hx==tx;
-                    boolean X = hy==ty;
-                    if(!(X||Y))X = Y = true;
-                    if(X)tx+=Math.signum(hx-tx);
-                    if(Y)ty+=Math.signum(hy-ty);
+                rx[0]+=dx;
+                ry[0]+=dy;
+                for(int j = 1; j<len; j++){
+                    int hx = rx[j-1];
+                    int hy = ry[j-1];
+                    int tx = rx[j];
+                    int ty = ry[j];
+                    int dst = Math.max(Math.abs(hx-tx),Math.abs(hy-ty));
+                    if(dst>1){//too far away! move closer!
+                        boolean Y = hx==tx;
+                        boolean X = hy==ty;
+                        if(!(X||Y))X = Y = true;
+                        if(X)tx+=Math.signum(hx-tx);
+                        if(Y)ty+=Math.signum(hy-ty);
+                        rx[j] = tx;
+                        ry[j] = ty;
+                    }
                 }
-                System.out.println(hx+" "+hy+"  "+tx+" "+ty);
-                visited.add(tx+","+ty);
+                visited.add(rx[len-1]+","+ry[len-1]);
             }
         }
         System.out.println(visited.size());
