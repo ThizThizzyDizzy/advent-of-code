760346d2e612d4a320c67d1a8690eb8ee6a15b86
ThizThizzyDizzy <thizthizzydizzy@gmail.com>
2022-12-15 13:00:52 -0500

    Day 15 part 1 - Java

--- src/aoc2022/java/Main.java
+++ src/aoc2022/java/Main.java
@@ -17,6 +17,7 @@ public class Main{
         days[11] = new Day12();
         days[12] = new Day13();
         days[13] = new Day14();
+        days[14] = new Day15();
     }
     public static void main(String[] args){
         for(int i = 0; i<25; i++){
---
+++ src/aoc2022/java/days/Day15.java
@@ -0,0 +1,68 @@
+package aoc2022.java.days;
+import aoc2022.java.Day;
+import java.util.ArrayList;
+public class Day15 extends Day{
+    private static int taxi(int x, int y, int x0, int y0){
+        return Math.abs(x-x0)+Math.abs(y-y0);
+    }
+    public Day15(){
+        super(15);
+    }
+    static int xmin = Integer.MAX_VALUE, xmax = Integer.MIN_VALUE, ymin = Integer.MAX_VALUE, ymax = Integer.MIN_VALUE;
+    static ArrayList<Sensor> sensors = new ArrayList<>();
+    @Override
+    public void run(){
+        for(String s : input.split("\n")){
+            //Sensor at x=545406, y=2945484: closest beacon is at x=772918, y=2626448
+            String[] str = s.replace(",", "").replace(":", "").split(" ");
+            int sx = Integer.parseInt(str[2].substring(2));
+            int sy = Integer.parseInt(str[3].substring(2));
+            int bx = Integer.parseInt(str[8].substring(2));
+            int by = Integer.parseInt(str[9].substring(2));
+            new Sensor(sx, sy, taxi(sx, sy, bx, by), bx, by);
+        }
+        int i = 0;
+        for(int x = xmin; x<=xmax; x++){
+            if(has(x,2000000))i++;
+        }
+        System.out.println(i);
+    }
+    public static boolean has(int x, int y){
+        if(x<xmin||x>xmax||y<ymin||y>ymax)return false;
+        for(Sensor s : sensors){
+            if(s.bx==x&&s.by==y)return false;
+        }
+        for(Sensor s : sensors){
+            if(taxi(s.x,s.y,x,y)<=s.radius){
+                return true;
+            }
+        }
+        return false;
+    }
+    private static class Sensor {
+        private final int x, y, radius;
+        private final int bx;
+        private final int by;
+        public Sensor(int x, int y, int radius, int bx, int by){
+            this.x = x;
+            this.y = y;
+            this.radius = radius;
+            xmin = Math.min(xmin, x-radius);
+            ymin = Math.min(ymin, y-radius);
+            xmax = Math.max(xmax, x+radius);
+            ymax = Math.max(ymax, y+radius);
+            sensors.add(this);
+            this.bx = bx;
+            this.by = by;
+        }
+        private int overlap(Sensor other){
+            int totalDist = taxi(x, y, other.x, other.y);
+            int depth = (radius+other.radius)-totalDist+1;
+            int width = (totalDist-depth+1)/2-Math.abs(radius-other.radius)*2+Math.max(radius,other.radius);
+            int odds = ((width+1)/2)*((depth+1)/2);
+            int evens = (width/2)*(depth/2);
+            System.out.println(width+" "+depth);
+            return odds+evens;
+        }
+    }
+}
